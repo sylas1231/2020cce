9b9e6b09246a8b88e80a87293ec62d7b05d6077d
sylas1231 <79677272+sylas1231@users.noreply.github.com>
2021-05-24 17:29:56 +0800

    Add files via upload

---
+++ HW12/HW12-1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+char line[2000];
+int main()
+{
+	for( int t=0; gets(line); t++ )
+	{
+		int ans[256]={};
+
+		for( int i=0; line[i]!=0; i++ )
+		{
+			char c=line[i];
+			ans[c]++;
+		}
+
+		if( t>0 ) printf("\n");
+		for( int i=0; i<256; i++ )
+		{
+			if( ans[i]>0 ) printf("%d %d\n", i, ans[i]);
+		}
+	}
+}
